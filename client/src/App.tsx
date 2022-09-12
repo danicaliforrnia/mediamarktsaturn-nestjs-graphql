cfbde13acfe2936e1188de230af98ea5d7879d1a
Daniel Stefanelli <d1as2n3i4e5l6>
2022-09-12 17:47:28 +0200

    feat: search order when filling input with an id and display json result

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,52 @@
-import { Box, Button, Card, CardContent, CardHeader, Grid, TextField } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Grid, TextField } from '@mui/material';
 import React, { useState } from 'react';
 import './App.css';
+import { useLazyQuery } from '@apollo/client';
+import BoxContainer from './components/BoxContainer';
+import JsonText from './components/JsonText';
+import { GET_ORDER_BY_ORDER_NUMBER_QUERY, OrderData, OrderVariable } from './graphql/queries';
 
 function App() {
-    const [order, setOrder] = useState(null);
+    const [getOrder, {loading, data, error}] = useLazyQuery<OrderData, OrderVariable>(GET_ORDER_BY_ORDER_NUMBER_QUERY);
+    const [orderId, setOrderId] = useState<number>();
     return (
         <div className="App">
             <body className="app-body">
-            <Card sx={{minWidth: '50vw', minHeight: '50vh'}}>
+            <Card sx={{minWidth: '40vw', minHeight: '40vh'}}>
                 <CardHeader
                     title=" Search Order"
                     subheader="MediaMarktSaturn"
                 />
                 <CardContent>
-                    <Grid container direction="column" spacing={2}>
+                    <Grid container direction="column" spacing={2} justifyContent="center" alignItems="center">
                         <Grid item container>
                             <Grid item xs={8}>
-                                <TextField label="Order Number" variant="outlined" placeholder="type an order number" fullWidth/>
+                                <TextField label="Order Number"
+                                           variant="outlined"
+                                           placeholder="type an order number"
+                                           onChange={e => setOrderId(+e.target.value)}
+                                           fullWidth/>
                             </Grid>
                             <Grid item xs={4}>
-                                <Button variant="contained">Search</Button>
+                                <Button variant="contained"
+                                        onClick={() => getOrder({variables: {id: orderId}})}
+                                        disabled={loading || !orderId}>Search</Button>
                             </Grid>
                         </Grid>
-                        {order && <Grid item>
-                            <Box
-                                component="span"
-                                sx={{
-                                    display: 'block',
-                                    p: 1,
-                                    bgcolor: '#282c34',
-                                    color: 'grey.300',
-                                    border: '1px solid',
-                                    borderColor: 'grey.800',
-                                    borderRadius: 2,
-                                    fontSize: '0.875rem',
-                                    fontWeight: '700',
-                                }}
-                            >
-                                block
-                            </Box>
+                        {loading && <Grid item>
+                            <BoxContainer>
+                                Loading data...
+                            </BoxContainer>
+                        </Grid>}
+                        {data && <Grid item width="100%">
+                            <BoxContainer>
+                                <JsonText data={data.order}/>
+                            </BoxContainer>
+                        </Grid>}
+                        {error && <Grid item width="100%">
+                            <BoxContainer>
+                                <JsonText data={error || 'Something bad happened, try again later :('}/>
+                            </BoxContainer>
                         </Grid>}
                     </Grid>
                 </CardContent>
